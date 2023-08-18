5b1154f2c4b5b4ed3f6ee053154d8b26d7696673
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-08-18 14:59:32 -0700

    Add end time to pick up session

--- templates/dreamdinners/customer/subtemplate/event_theme/standard/made_for_you/standard/default/header.tpl.php
+++ templates/dreamdinners/customer/subtemplate/event_theme/standard/made_for_you/standard/default/header.tpl.php
@@ -5,7 +5,10 @@
 		<p>Want to assemble your meals in our store? <a href="/main.php?static=how_it_works">Learn more</a> about how our assembly sessions work.</p>
 		<div class="row m-2 p-3 bg-gray">
 			<div class="col text-center">
-				<p class="font-weight-bold"><?php echo CTemplate::dateTimeFormat($this->session['session_start'], VERBOSE); ?></p>
+				<p class="font-weight-bold">
+					<?php echo CTemplate::dateTimeFormat($this->session['session_start'], VERBOSE); ?>
+					to <?php echo CTemplate::dateTimeFormat($this->session['session_end'], TIME_ONLY); ?>
+				</p>
 				<p class="font-weight-bold mb-0">Dream Dinners <?php echo $this->session['store_name']; ?></p>
 				<p class="font-weight-bold mb-0"><?php echo $this->cart_info['store_info']['address_line1']; ?><?php echo (!empty($this->cart_info['store_info']['address_line2'])) ? ' ' . $this->cart_info['store_info']['address_line2'] : ''; ?> <?php echo $this->cart_info['store_info']['city'];?>, <?php echo $this->cart_info['store_info']['state_id']; ?></p>
 			</div>
