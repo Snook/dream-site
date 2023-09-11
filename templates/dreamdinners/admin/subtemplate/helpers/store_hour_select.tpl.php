e7d666027cbf8fa76997ba0e435d82cc2ca41e54
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-09-11 13:44:37 -0700

    No _html for hidden form elements, located in hidden_html

--- templates/dreamdinners/admin/subtemplate/helpers/store_hour_select.tpl.php
+++ templates/dreamdinners/admin/subtemplate/helpers/store_hour_select.tpl.php
@@ -22,11 +22,6 @@
 	</tr>
 	<tr>
 		<td class="bgcolor_light" colspan="2">
-			<?php echo $this->form_store_details['bio_store_hours_html']; ?>
-		</td>
-	</tr>
-	<tr>
-		<td class="bgcolor_light" colspan="2">
 			<div style="border: 2px solid #a8a94c; padding: 4px; display: none;" id="bio_store_hours_preview"></div>
 		</td>
 	</tr>
