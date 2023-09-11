5fc4232b97c5ce149a9e30e35665f100253c2616
Brandy Latta <brandy.latta@dreamdinners.com>
2023-09-11 13:08:22 -0700

    playing with nav

--- templates/dreamdinners/customer/subtemplate/store/store_navigation.tpl.php
+++ templates/dreamdinners/customer/subtemplate/store/store_navigation.tpl.php
@@ -26,3 +26,36 @@
 		</ul>
 	</div>
 </div>
+
+<div class="row">
+	<div class="col text-center">
+		<ul class="nav justify-content-around justify-content-md-between">
+			<li class="nav-item mb-3">
+				<i class="dd-icon icon-location font-size-extra-extra-large text-green-light m-4"></i>
+				<a class="m-auto nav-link text-uppercase font-weight-bold" style="width: 10rem; height: 10rem;" href="<?php echo $this->DAO_store->getPrettyUrl(); ?>">
+					<span>Location info</span>
+				</a>
+			</li>
+			<?php if ($this->DAO_store->hasBioPage()) { ?>
+				<li class="nav-item mb-3">
+					<i class="dd-icon icon-friend font-size-extra-extra-large text-cyan-dark m-4"></i>
+					<a class="m-auto nav-link text-uppercase font-weight-bold" style="width: 10rem; height: 10rem;" href="<?php echo $this->DAO_store->getPrettyUrl(); ?>/meet-the-owner">
+						<span>Meet the owner</span>
+					</a>
+				</li>
+			<?php } ?>
+			<li class="nav-item mb-3">
+				<i class="dd-icon icon-calendar_add font-size-extra-extra-large text-orange m-4"></i>
+				<a class="m-auto nav-link text-uppercase font-weight-bold" style="width: 10rem; height: 10rem;" href="<?php echo $this->DAO_store->getPrettyUrl(); ?>/calendar">
+					<span>What's New &amp; Store Calendar</span>
+				</a>
+			</li>
+			<li class="nav-item mb-3">
+				<i class="dd-icon icon-order_online font-size-extra-extra-large text-green-dark m-4"></i>
+				<a class="m-auto nav-link text-uppercase font-weight-bold" style="width: 10rem; height: 10rem;" href="<?php echo $this->DAO_store->getPrettyUrl(); ?>/order">
+					<span>Order Now</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
