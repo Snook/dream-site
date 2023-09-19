004c4ed9914a200fd0e72c65ed26cefa2587bc79
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-09-19 12:48:23 -0700

    Add test script

--- test/test_EmailDelivered.php
+++ test/test_EmailDelivered.php
@@ -7,13 +7,13 @@ require_once('includes/DAO/BusinessObject/COrders.php');
  *  Test order confirmation email
  */
 $DAO_orders = DAO_CFactory::create('orders', true);
-$DAO_orders->id = 3839619;
+$DAO_orders->id = 3816265;
 $DAO_orders->find(true);
 
 $DAO_user = DAO_CFactory::create('user', true);
 $DAO_user->id = $DAO_orders->user_id;
 $DAO_user->find(true);
 
-COrders::sendConfirmationEmail($DAO_user, $DAO_orders);
+COrdersDelivered::sendConfirmationEmail($DAO_user, $DAO_orders);
 
 ?>
