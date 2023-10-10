e66b7679ea5be1ca316a79df65c49fd09e95029e
Evan Lee <evan.lee@dreamdinners.com>
2023-10-10 13:19:24 -0700

    Rebuild analytics js

--- src/www/theme/dreamdinners/js/customer/analytics.js
+++ src/www/theme/dreamdinners/js/customer/analytics.js
@@ -224,7 +224,7 @@ var _etmc = _etmc || []; // salesforce
 			});
 		}
 
-		// iUbenda
+		// iUbenda new
 		_iub.csConfiguration = {"cookiePolicyInOtherWindow":true,"countryDetection":true,"enableGdpr":false,"enableUspr":true,"lang":"en","siteId":1685553,"showBannerForUS":true,"usprPurposes":"s,sh,adv","whitelabel":false,"cookiePolicyId":48982534, "banner":{"backgroundColor":"#4c4c4c","closeButtonRejects":true,"position":"float-bottom-left","textColor":"white"}};
 		(function() {
 			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = false;
