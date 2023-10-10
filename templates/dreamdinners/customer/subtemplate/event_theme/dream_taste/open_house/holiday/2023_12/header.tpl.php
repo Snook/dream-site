c9a649a229d2cffeba6c589e27dccfe788c2392e
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2023-10-10 15:57:31 -0700

    new and updated

--- templates/dreamdinners/customer/subtemplate/event_theme/dream_taste/open_house/holiday/2023_12/header.tpl.php
+++ templates/dreamdinners/customer/subtemplate/event_theme/dream_taste/open_house/holiday/2023_12/header.tpl.php
@@ -1,7 +1,7 @@
 <div class="row mb-5">
 	<div class="col-12 col-lg-6">
 		<h3>Celebrate the season with our Holiday Breakfast Trial Offer</h3>
-		<p><span class="font-weight-bold">For only $99.99, Dream Dinners has you covered for both your holiday morning brunch and the week that follows. During this exclusive pick up event, you will receive a Bacon Breakfast Frittata, Cinnamon Streusel French Toast Bake and Country Potatoes plus three medium dinners from our delicious menu.</p>
+		<p><span class="font-weight-bold">For only $92.99, Dream Dinners has you covered for both your holiday morning brunch and the week that follows. During this exclusive pick up event, you will receive a Bacon Breakfast Frittata, Cinnamon Streusel French Toast Bake and Country Potatoes plus three medium dinners from our delicious menu.</p>
 		<p>We’re excited to introduce you to the Dream Dinners experience and make your holiday homemade, made easy. Our offer is only for guests of this event, and spaces are limited. Use the link below to reserve your spot today!</p>
 		<p>*Each item in the Holiday Breakfast serves 2-3 people.</p>
 		<div class="row m-2 p-3 bg-gray">
