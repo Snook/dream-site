9f6fe8b47438ca695f1cb9d0c060d16413fb4508
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-10-11 14:51:52 -0700

    Create find_DAO_orders

---
+++ test/test_find_DAO_order.php
@@ -0,0 +1,24 @@
+<?php
+require_once("../includes/Config.inc");
+require_once("DAO.inc");
+
+
+$DAO_orders = DAO_CFactory::create('orders', true);
+
+$DAO_orders->id = 3851043;
+//$DAO_orders->whereAdd('orders.id IN (3851040, 3851043)');
+//$DAO_orders->user_id = 662598;
+
+$DAO_orders->orderBy("orders.timestamp_created DESC");
+$DAO_orders->find_DAO_orders();
+
+$ordersArray = array();
+while($DAO_orders->fetch())
+{
+	$ordersArray[$DAO_orders->id] = clone $DAO_orders;
+	$ordersArray[$DAO_orders->id]->fetch_DAO_order_item_Array();
+	$ordersArray[$DAO_orders->id]->fetch_DAO_payment_Array();
+}
+
+$debugbreakpoint = true;
+?>
