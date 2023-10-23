7d753c0be995190e8f916a6aac69412c0247b2bc
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-10-23 15:14:50 -0700

    Remove duplicate, exists in analytics.js

--- templates/dreamdinners/customer/subtemplate/page_footer/page_footer_analytics.tpl.php
+++ templates/dreamdinners/customer/subtemplate/page_footer/page_footer_analytics.tpl.php
@@ -17,8 +17,3 @@
 	<?php } ?>
 	<img src="https://cts.vresp.com/s.gif?h=46b82cf4cc&amount=<?php echo $this->head_analytics_array['total']; ?>" height="1" width="1" />
 <?php } ?>
-
-<?php if (!empty($this->head_analytics_array['smfcArray'])) { ?>
-	<!--salesforce pixel-->
-	<img src="//click.s10.exacttarget.com/conversion.aspx?xml=<system><system_name>tracking</system_name><action>conversion</action><member_id><?php echo $this->head_analytics_array['smfcArray']['mid']; ?></member_id><job_id><?php echo $this->head_analytics_array['smfcArray']['j']; ?></job_id><sub_id><?php echo $this->head_analytics_array['smfcArray']['sfmc_sub']; ?></sub_id><list><?php echo $this->head_analytics_array['smfcArray']['l']; ?></list><original_link_id><?php echo $this->head_analytics_array['smfcArray']['u']; ?></original_link_id><BatchID><?php echo $this->head_analytics_array['smfcArray']['jb']; ?></BatchID><conversion_link_id>1</conversion_link_id><link_alias><?php echo (!empty($this->page_title)) ? $this->page_title : 'DreamDinners.com'; ?></link_alias><display_order>3</display_order><email></email><data_set></data_set></system>" width="1" height="1">
-<?php } ?>
