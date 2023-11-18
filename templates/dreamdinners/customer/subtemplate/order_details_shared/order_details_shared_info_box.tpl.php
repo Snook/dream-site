0ec86a48da6fb6a00c6a7ac02db2a54e3b277be5
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-11-17 16:40:31 -0800

    Disable ability to edit order

--- templates/dreamdinners/customer/subtemplate/order_details_shared/order_details_shared_info_box.tpl.php
+++ templates/dreamdinners/customer/subtemplate/order_details_shared/order_details_shared_info_box.tpl.php
@@ -12,12 +12,7 @@
 		<a class="btn btn-secondary btn-sm d-print-none" href="/session?reschedule=<?php echo $this->orderInfo['id']; ?>">Reschedule</a>
 	<?php } ?>
 	<?php if ($this->orderDetailsArray['bookingStatus'] != CBooking::CANCELLED) { ?>
-		<?php if ( $this->orderDetailsArray['canEditDeliveredOrder'] ) { ?>
-			<a href="/checkout?restore_order=<?php echo $this->orderInfo['id']; ?>" class="btn btn-primary">Edit Order</a>
-			<button id="handle-cancel-delivered-order" data-user_id="<?php echo $this->orderInfo['user_id']; ?>" data-store_id="<?php echo $this->storeInfo['id']; ?>" data-session_id="<?php echo $this->sessionInfo['id']; ?>" data-order_id="<?php echo $this->orderInfo['id']; ?>" data-menu_id="<?php echo $this->menuInfo['menu_id']; ?>" class="btn btn-primary btn-gray">Cancel Order</button>
-		<?php } else{ ?>
-			<p class="mt-2 font-size-small">To edit your order or make additional changes please contact Dream Dinners at <a href="mailto:support@dreamdinners.com?subject=Dream Dinners Delivered support request, confirmation number <?php echo $this->orderDetailsArray['orderInfo']['order_confirmation']; ?>">support@dreamdinners.com</a></p>
-		<?php } ?>
+		<p class="mt-2 font-size-small">To edit your order or make additional changes please contact Dream Dinners at <a href="mailto:support@dreamdinners.com?subject=Dream Dinners Delivered support request, confirmation number <?php echo $this->orderDetailsArray['orderInfo']['order_confirmation']; ?>">support@dreamdinners.com</a></p>
 	<?php } ?>
 	<?php if ( !empty($this->orderInfo['orderShipping']['tracking_number'])) { ?>
 		<div class="font-weight-bold font-size-small">UPS Tracking Number: <a target="_blank" href="<?php echo CAppUtil::upsTrackingUrl($this->orderInfo['orderShipping']['tracking_number']);?>"><?php echo $this->orderInfo['orderShipping']['tracking_number']; ?></a></div>
