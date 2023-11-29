4dc76e2c644eeeca6f07094733091102f8f9a486
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-11-29 10:16:59 -0800

    Remove Mariah at request of Sarah Miller

--- includes/DAO/BusinessObject/CGiftCard.php
+++ includes/DAO/BusinessObject/CGiftCard.php
@@ -1514,7 +1514,7 @@ class CGiftCard extends DAO_Gift_card_transaction
 
 		$subject = "Dream Dinners Card Orders Report for " . date("m-d-y");
 		$Mail = new CMail();
-		$Mail->to_email = "sarah@smarttransactions.com, mariah@localgiftcards.com, geekifyinc@gmail.com";
+		$Mail->to_email = "sarah@smarttransactions.com, geekifyinc@gmail.com";
 		$Mail->cc_email = "ryan.snook@dreamdinners.com";
 		$Mail->subject = $subject;
 
