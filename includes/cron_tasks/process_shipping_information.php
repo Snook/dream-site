2109162eea41263da6c64d5331b63e3c85d90d72
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-11-29 17:20:04 -0800

    Add cron log update

--- includes/cron_tasks/process_shipping_information.php
+++ includes/cron_tasks/process_shipping_information.php
@@ -4,7 +4,22 @@ require_once("CLog.inc");
 require_once("includes/api/shipping/shipstation/ShipStationManager.php");
 require_once("includes/api/shipping/shipstation/ShipStationOrderBatchWrapper.php");
 
-ShipStationManager::loadOrderShippingInfo();
+try
+{
+	if (defined("DISABLE_CRON") && DISABLE_CRON)
+	{
+		CLog::Record("CRON: process_shipping_information called but cron is disabled");
+		CLog::RecordCronTask(1, CLog::FAILURE, CLog::PROCESS_SHIPPING_INFORMATION, "process_shipping_information called but cron is disabled.");
+		exit;
+	}
 
+	ShipStationManager::loadOrderShippingInfo();
 
+	CLog::RecordCronTask(1, CLog::SUCCESS, CLog::PROCESS_SHIPPING_INFORMATION, "Processed shipping information.");
+}
+catch (exception $e)
+{
+	CLog::RecordCronTask(1, CLog::PARTIAL_FAILURE, CLog::PROCESS_SHIPPING_INFORMATION, "process_shipping_information: Exception occurred: " . $e->getMessage());
+	CLog::RecordException($e);
+}
 ?>
