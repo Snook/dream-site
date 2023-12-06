c39fecb00149d34fcaf891c1d3174d9be28fa5ef
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2023-12-05 17:18:24 -0800

    updated contact info

--- templates/dreamdinners/admin/help/help_gift_card_management_load.tpl.php
+++ templates/dreamdinners/admin/help/help_gift_card_management_load.tpl.php
@@ -2,7 +2,7 @@
   <tr>
     <td>
 
- Loading Funds
+ <p>Loading Funds</p>
 <ul>
   <li>Funds can be loaded onto traditional (plastic) cards or existing virtual eGift card accounts.</li>
   <li>You will need to have the full Gift Card number in order to &quot;load&quot; funds directly onto a card account.</li>
@@ -14,15 +14,16 @@
   <li>If the email address field is filled in with a valid email address, the system will send a receipt to the address provided. This is optional, if no email is entered, no email will be sent.</li>
   <li>Please PRINT a receipt for the customer after purchase, this is their &quot;Proof of Purchase&quot;. This is especially important if no email address is entered. </li>
 </ul>
-<p>Franchise Gift Card Support at 1to1 Card</p>
+<p>Franchise Gift Card Support at Smart Transactions</p>
 <ul>
-  <li>Phone: 630-754-4800, 8:30a.m. - 5:30p.m. CST</li>
-  <li>Email: <a href="mailto:orders@1to1card.com">orders@1to1card.com</a></li>
+  <li>Phone: 888-494-9760 x 2</li>
+  <li>Email: <a href="mailto:info@smarttransactions.com">info@smarttransactions.com</a></li>
+  <li>Web: <a href="https://www.smarttransactionsystems.com/support/">https://www.smarttransactionsystems.com/support/</a></li>
 </ul>
 <p>Customer Gift Card Support </p>
 <ul>
-  <li>24/7 Automated cardholder balance information: 1-360-804-2020</li>
-  <li>Cardholder account balance information: <a href="/giftcards">gift cards</a></li>
+  <li>24/7 Automated cardholder balance information: 1-888-303-1175</li>
+  <li>Cardholder account balance information: <a href="https://smarttransactions.net/gb/">https://smarttransactions.net/gb/</a></li>
 </ul>
 </td>
   </tr>
