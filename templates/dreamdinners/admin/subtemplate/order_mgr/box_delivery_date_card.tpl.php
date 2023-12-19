6a226aaf9231ce7abc3f93a4f183f2a0c386e2e6
Ryan Snook <105461991+Snook@users.noreply.github.com>
2023-12-19 11:37:28 -0800

    box delivery date style update

--- templates/dreamdinners/admin/subtemplate/order_mgr/box_delivery_date_card.tpl.php
+++ templates/dreamdinners/admin/subtemplate/order_mgr/box_delivery_date_card.tpl.php
@@ -1,15 +1,17 @@
 <div class="row mb-2">
-	<div class="col list-group text-center">
-		<?php foreach ($day['sessions'] AS $id => $session) {
-			if ($id == $this->orgSessionID) { ?>
-			<button type="button" class="btn btn-primary btn-spinner list-group-item list-group-item-action disabled" style="color:white !important; padding:14px 6px;" data-selected_delivery_day="<?php echo $session['id']; ?>">&nbsp;(Selected)
-				<?php echo CTemplate::dateTimeFormat($date, FULL_DAY); ?> <?php echo CTemplate::dateTimeFormat($date, FULL_MONTH_DAY_YEAR); ?>
+	<div class="col-8 m-auto text-center">
+		<?php foreach ($day['sessions'] AS $id => $session) { ?>
+			<button type="button" class="btn btn-green btn-sm w-100 btn-spinner py-3 <?php if ($id == $this->orgSessionID) { ?>disabled<?php } ?>"
+					data-select_delivery_day="<?php echo $session['id']; ?>"
+					data-date="<?php echo CTemplate::dateTimeFormat($date, FULL_DAY); ?> <?php echo CTemplate::dateTimeFormat($date, FULL_MONTH_DAY_YEAR); ?>"
+					data-booked_count="<?php echo $session["booked_count"]; ?>"
+					data-remaining_slots="<?php echo $session["remaining_slots"]; ?>">
+					<span class="row">
+						<span class="col-3"><span class="collapse <?php if ($id == $this->orgSessionID) { ?>show<?php } ?>">(Selected)</span></span>
+						<span class="col-6"><?php echo CTemplate::dateTimeFormat($date, FULL_DAY); ?> <?php echo CTemplate::dateTimeFormat($date, FULL_MONTH_DAY_YEAR); ?></span>
+						<span class="col-3"><span data-toggle="tooltip" data-placement="top" title="Booked / Available">(<?php echo $session["booked_count"]; ?>/<?php echo $session["remaining_slots"]; ?>)</span></span>
+					</span>
 			</button>
-		<?php } else { ?>
-				<button type="button" class="btn btn-primary btn-spinner list-group-item list-group-item-action" style="color:black !important; background-image:none; background-color: #9dd8d8; padding:14px 6px;" data-select_delivery_day="<?php echo $session['id']; ?>"
-					data-date="<?php echo CTemplate::dateTimeFormat($date, FULL_DAY); ?> <?php echo CTemplate::dateTimeFormat($date, FULL_MONTH_DAY_YEAR); ?>" >
-					<?php echo CTemplate::dateTimeFormat($date, FULL_DAY); ?> <?php echo CTemplate::dateTimeFormat($date, FULL_MONTH_DAY_YEAR); ?>
-				</button>
-		<?php } }  ?>
+		<?php }  ?>
 	</div>
 </div>
