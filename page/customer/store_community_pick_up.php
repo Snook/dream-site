0dddee6f6555fa2484e4fac09ec2ba80497452ae
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-01-09 13:13:00 -0800

    Community pick up [WIP]

---
+++ page/customer/store_community_pick_up.php
@@ -0,0 +1,77 @@
+<?php
+require_once('DAO/BusinessObject/CStore.php');
+require_once('DAO/BusinessObject/CStatesAndProvinces.php');
+require_once('includes/DAO/BusinessObject/CMenu.php');
+
+class page_store_community_pick_up extends CPage
+{
+	function runPublic()
+	{
+		if (!empty($_GET['id']) && (is_numeric($_GET['id']) || CTemplate::isAlphaNumHyphen($_GET['id'])))
+		{
+			$DAO_store = DAO_CFactory::create('store', true);
+			$DAO_store->id = $_GET['id'];
+			$DAO_store->show_on_customer_site = 1;
+
+			if ($DAO_store->find_DAO_store(true))
+			{
+				$DAO_store->getAvailableJobsArray();
+
+				$locationArray = array();
+
+				foreach ($DAO_store->getStorePickupLocations() AS $location)
+				{
+					// Prime with active locations
+					if ($location->is_Active())
+					{
+						$locationArray[$location->id] = array(
+							'DAO_store_pickup_location' => clone $location,
+							'sessionArray' => null
+						);
+					}
+				}
+
+				$DAO_session = DAO_CFactory::create('session', true);
+				$DAO_session->store_id = $DAO_store->id;
+				$DAO_session->session_publish_state = CSession::PUBLISHED;
+				$DAO_session->whereAdd("menu.id IN (" . implode(',', CMenu::getActiveMenuArrayIDs()) . ")");
+				$DAO_session->whereAdd("session_properties.store_pickup_location_id IS NOT NULL");
+				$DAO_session->orderBy("store_pickup_location.state_id, store_pickup_location.city, session.session_start");
+
+				$DAO_session->find_DAO_session();
+
+				while ($DAO_session->fetch())
+				{
+					if ($DAO_session->isOpen() && !empty($locationArray[$DAO_session->DAO_store_pickup_location->id]))
+					{
+						// Need to add location if there is a session active but the location itself is now inactive
+						// An inactive location does not govern session availability
+						if (empty($locationArray[$DAO_session->DAO_store_pickup_location->id]))
+						{
+							$locationArray[$location->id] = array(
+								'DAO_store_pickup_location' => clone $DAO_session->DAO_store_pickup_location,
+								'sessionArray' => null
+							);
+						}
+
+						$locationArray[$DAO_session->DAO_store_pickup_location->id]['sessionArray'][$DAO_session->id] = clone $DAO_session;
+					}
+				}
+
+				$this->Template->assign('DAO_store', $DAO_store);
+				$this->Template->assign('locationArray', $locationArray);
+			}
+			else
+			{
+				$this->Template->setStatusMsg('Sorry, it appears the location you are looking for has permanently closed. Please enter your zip code below to another location near you or see if our shipping service is available.');
+				CApp::bounce('/locations');
+			}
+		}
+		else
+		{
+			CApp::bounce('/locations');
+		}
+	}
+}
+
+?>
---
+++ templates/dreamdinners/customer/store_community_pick_up.tpl.php
@@ -0,0 +1,132 @@
+<?php $this->setPreload(IMAGES_PATH . "/stores/store-landing.jpg", "image"); ?>
+<?php $this->assign('page_title', htmlspecialchars($this->DAO_store->store_name) . " - Community Pick Up locations"); ?>
+<?php $this->assign('canonical_url', $this->DAO_store->getPrettyUrl(true) . '/community-pick-up'); ?>
+<?php $this->assign('order_process_navigation_page', 'session-menu'); ?>
+<?php include $this->loadTemplate('customer/subtemplate/page_header.tpl.php'); ?>
+
+	<header class="container my-5">
+		<div class="row mb-3">
+			<div class="col text-center">
+				<h1><?php echo $this->DAO_store->store_name; ?> Dream Dinners Community Pick Up locations</h1>
+				<h3 class="font-marker">We offer real food, made from scratch, so your life can feel a little easier.</h3>
+			</div>
+		</div>
+		<?php include $this->loadTemplate('customer/subtemplate/store/store_navigation.tpl.php'); ?>
+	</header>
+
+	<section>
+		<div class="container mb-5">
+
+			<div class="row">
+				<div class="col">
+					<h3 class="text-uppercase font-weight-bold text-center">Communities served by <?php echo $this->DAO_store->store_name; ?></h3>
+				</div>
+			</div>
+			<div class="row">
+				<?php foreach ($this->locationArray AS $location) { ?>
+					<div class="col m-auto text-center">
+						<a href="#<?php echo $location['DAO_store_pickup_location']->generateAnchor(); ?>" class="btn btn-link mb-2"><?php echo $location['DAO_store_pickup_location']->city; ?></a>
+					</div>
+				<?php } ?>
+			</div>
+
+			<hr class="border-green-light border-width-3-5-imp my-5 border-top-style-dotted">
+
+			<?php foreach ($this->locationArray AS $location) { ?>
+				<div class="row mb-4">
+					<div class="col-12 col-lg-6 col-xl-5">
+						<img src="<?php echo IMAGES_PATH; ?>/style/locations/community-pick-up.jpg" alt="<?php echo $this->DAO_store->store_name; ?>" class="img-fluid w-100">
+					</div>
+					<div class="text-center text-lg-left col-12 mt-3 mt-lg-0 col-lg-6 col-xl-3">
+						<h3 id="<?php echo $location['DAO_store_pickup_location']->generateAnchor(); ?>" class="text-uppercase font-weight-bold"><?php echo $location['DAO_store_pickup_location']->city; ?></h3>
+						<?php echo $location['DAO_store_pickup_location']->generateAddressHTML(); ?>
+					</div>
+					<div class="col-12 mt-3 col-xl-4 mt-xl-0">
+						<h3 class="text-uppercase font-weight-bold text-center text-md-left">
+							Location info
+						</h3>
+						<div class="location-about">
+							<?php echo $location['DAO_store_pickup_location']->location_title; ?>
+						</div>
+					</div>
+				</div>
+
+				<div class="row mb-3">
+					<div class="col">
+						<iframe
+								class="border border-width-2-imp border-gray-500"
+								width="100%"
+								height="250"
+								src="//www.google.com/maps/embed/v1/place?key=<?php echo GOOGLE_APIKEY; ?>&q=<?php echo urlencode($location['DAO_store_pickup_location']->generateAddressLinear()); ?>" allowfullscreen>
+						</iframe>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col">
+						<h3 class="text-uppercase font-weight-bold text-center">Available pick up times</h3>
+					</div>
+				</div>
+				<div class="row">
+					<?php if (empty($location["sessionArray"])) { ?>
+						<div class="col">
+							<p class="text-center">No community pick up times currently available for the <?php echo $location['DAO_store_pickup_location']->city; ?> location.</p>
+						</div>
+					<?php } else { ?>
+						<?php foreach ($location["sessionArray"] AS $DAO_session) { ?>
+							<div class="col-md-6 m-auto">
+								<a href="/session/<?php echo $DAO_session->id; ?>" class="btn btn-primary btn-block mb-2" rel="nofollow"><?php echo CTemplate::dateTimeFormat($DAO_session->session_start, VERBOSE); ?></a>
+							</div>
+						<?php } ?>
+					<?php } ?>
+				</div>
+
+				<hr class="border-green-light border-width-3-5-imp my-5 border-top-style-dotted">
+
+			<?php } ?>
+
+			<div class="row no-gutters">
+				<div class="col-12 col-lg-6">
+					<div class="card-group text-center">
+						<div class="card border-0">
+							<img src="<?php echo IMAGES_PATH; ?>/landing_pages/order-online-no-text-circles-550x410.webp" alt="order online" class="img-fluid" />
+							<div class="card-body">
+								<h5 class="card-title my-0">1. Order Online</h5>
+								<p class="card-text">View our monthly menu, select a time and complete your order.</p>
+							</div>
+						</div>
+						<div class="card border-0">
+							<img src="<?php echo IMAGES_PATH; ?>/landing_pages/prep-meals-no-text-circles-550x410.webp" alt="prep your meals" class="img-fluid" />
+							<div class="card-body">
+								<h5 class="card-title my-0">2. Meals are Prepped</h5>
+								<p class="card-text">The shopping, chopping, prep and clean up are taken care of, so you can enjoy meals at home.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-12 col-lg-6">
+					<div class="card-group text-center">
+						<div class="card border-0">
+							<img src="<?php echo IMAGES_PATH; ?>/landing_pages/cook-at-home-no-text-circles-550x410.webp" alt="cook at home" class="img-fluid" />
+							<div class="card-body">
+								<h5 class="card-title my-0">3. Cook at Home</h5>
+								<p class="card-text">Thaw your meals each week, cook as directed, and enjoy dinner together.</p>
+							</div>
+						</div>
+						<div class="card border-0">
+							<img src="<?php echo IMAGES_PATH; ?>/landing_pages/eat-connect-no-text-circles-550x410.webp" alt="eat and connect together" class="img-fluid" />
+							<div class="card-body">
+								<h5 class="card-title my-0">4. Eat and Connect</h5>
+								<p class="card-text">Spend less time in the kitchen and more time doing what you love.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</section>
+
+<?php include $this->loadTemplate('customer/subtemplate/store/store_footer.tpl.php'); ?>
+
+<?php include $this->loadTemplate('customer/subtemplate/page_footer.tpl.php'); ?>
