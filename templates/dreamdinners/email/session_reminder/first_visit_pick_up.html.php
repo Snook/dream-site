38205848aecf685afd67d42e97d0873b4bd0f394
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-02-02 14:37:57 -0800

    new session reminder emails

---
+++ templates/dreamdinners/email/session_reminder/first_visit_pick_up.html.php
@@ -0,0 +1,7 @@
+<p><b>What to Expect:</b></p>
+<ol>
+<li>Your meals assembled and waiting for you.</li>
+<li>Our team ready to walk you through the process and answer any questions you have.</li>
+<li>Samples of upcoming menu items & the chance to place your next order.</li>
+<li>A team member to load your cooler (don’t forget yours!) and help bring it to your car.</li>
+</ol>
---
+++ templates/dreamdinners/email/session_reminder/session_tips.html.php
@@ -0,0 +1,11 @@
+<p><b>Tips</b></p>
+<table width="100%" border="0" cellspacing="0" cellpadding="5">
+<tr>
+	<td><ol>
+		<li>Add on a few of our delicious sides, breakfast and sweets <a href="<?=HTTPS_BASE?>freezer">from our Sides and Sweets Freezer.</a></li>
+		<li><a href="<?=HTTPS_BASE?>session-menu">Place your next order online.</a></li>
+	  	<li>Be on time.</li>
+		<li>Bring your cooler.</li></ol>
+	</td>
+</tr>
+</table>
