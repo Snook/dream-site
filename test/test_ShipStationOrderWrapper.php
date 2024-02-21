f29722c666226978feba2320363f50ac85a42524
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-02-20 16:49:46 -0800

    Remove Avalara functionality

--- test/test_ShipStationOrderWrapper.php
+++ test/test_ShipStationOrderWrapper.php
@@ -3,9 +3,9 @@ require_once(dirname(__FILE__) . "/../includes/Config.inc");
 require_once 'includes/api/shipping/shipstation/ShipStationManager.php';
 require_once 'includes/api/shipping/shipstation/ShipStationOrderWrapper.php';
 
-$order = new COrdersDelivered();
+$order = new COrdersDelivered(true);
 $order->id = 3650620;
-$order->find(true);
+$order->find_DAO_orders(true);
 $order->reconstruct();
 $order->orderAddress();
 
