1a9bc9fccbfadab5281fe56141e699fc0e93df5d
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-03-05 10:51:14 -0800

    Update FAQ page

--- templates/dreamdinners/customer/faqs.tpl.php
+++ templates/dreamdinners/customer/faqs.tpl.php
@@ -23,10 +23,10 @@
 				<div class="row">
 					<div class="col-md-8 mx-auto">
 						<h2 class="font-weight-semi-bold text-uppercase font-size-medium mb-4 text-center">What can we help you with?</h2>
-						<p class="mb-3 text-center">Browse our list of help topics outlined below. If you still have questions and would like to speak with a team member at your local store, click <a href="/locations">Locations</a> to find the direct contact information for the store that you are interested in. Additionally, you can contact our home office from our <a href="/contact-us">Contact Us page</a> for website support or guest services.</p>
+						<p class="mb-3 text-center">If you have questions and would like to speak with a team member at your local store, click <a href="/locations">Locations</a> to find the direct contact information for the store that you are interested in. Additionally, you can contact our home office from our <a href="/contact-us">Contact Us page</a> for website support or guest services.</p>
 
 						<?php if (defined('ENABLE_HELP_SEARCH') && ENABLE_HELP_SEARCH == true) { ?>
-							<a class="btn btn-primary btn-lg btn-block help-search-launcher" href="#">Launch Helpdesk</a>
+							<a class="btn btn-primary btn-lg btn-block help-search-launcher" href="#">Search Helpdesk</a>
 						<?php } ?>
 					</div>
 				</div>
