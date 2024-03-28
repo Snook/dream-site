c145fbaa36652df83d49984e478cf4da07085e79
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-03-28 16:52:06 -0700

    u[date

--- templates/dreamdinners/customer/shipping.tpl.php
+++ templates/dreamdinners/customer/shipping.tpl.php
@@ -33,7 +33,7 @@
 		<section>
 			<div class="container-fluid my-5">
 				<div class="row hero-double">
-					<div class="col-md-5 text-left p-5 my-5">
+					<div class="col-md-6 text-left p-5 my-5">
 						<h1 class="font-weight-bold font-marker mt-2">Get Dream Dinners Shipped to Your Door</h1>
 						<p class="text-uppercase mb-4">Real food made from scratch, so your life can feel a little easier.</p>
 						<p>Four easy, homemade meals prepped and ready to cook, most in less than 30 minutes. Order when you want, no subscription necessary. Enter your zip code to see if we can deliver to you. Great for travel or a gift for new baby, new home, mothers's day and more!</p>
@@ -51,7 +51,7 @@
 							</div>
 						</form>
 					</div>
-					<div class="col-md-7 p-0">
+					<div class="col-md-6 p-0">
 						<img src="<?php echo IMAGES_PATH; ?>/landing_pages/apr24-shipping-collage-957x657.webp" alt="April Menu" class="img-fluid">
 					</div>
 
@@ -111,7 +111,7 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col offset-md-2 col-md-8">
+					<div class="col offset-md-2 col-md-4 mx-lg-auto">
 					<h2 class="font-weight-bold font-marker mt-2">Ready to start cooking?</h2>
 						<p class="text-uppercase mb-3">Enter you zip code to see the menu.</p>
 						<form action="/locations" method="post" class="form-shipping-search needs-validation" novalidate>
@@ -133,14 +133,13 @@
 		</section>
 
 		<section>
+		<div class="border-top mb-3 mx-5" style="border-top: #b9bf33 dotted 5px !important;"></div>
 			<div class="container mb-5">
-				
-
 				<div class="row">
 					<div class="col">
 						<div class="text-center">
 							<h2 class="mt-5 mb-4 font-weight-bold">The Dream Dinners Difference</h2>
-							<!--<div class="border-bottom border-green-dark mb-3 mx-5"></div>-->
+							<div class="border-bottom border-green-dark mb-3 mx-5"></div>
 						</div>
 					</div>
 				</div>
