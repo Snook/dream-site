b1bc1a5edbfc40656733bbff16391547604c6be5
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-04-23 12:54:15 -0700

    No $5 limit yet

--- includes/DAO/BusinessObject/COrdersDelivered.php
+++ includes/DAO/BusinessObject/COrdersDelivered.php
@@ -2278,7 +2278,7 @@ class COrdersDelivered extends COrders
 
 	function getPointsDiscountableAmount()
 	{
-		$maxDeductible = $this->subtotal_menu_items + $this->subtotal_home_store_markup + $this->subtotal_service_fee;
+		$maxDeductible = parent::getPointsDiscountableAmount();
 
 		/*
 		if ($maxDeductible > 5.00)
