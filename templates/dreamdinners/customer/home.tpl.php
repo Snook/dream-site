a8484b258ae90b1e878ad605c11bf84e98de7d8d
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-04-23 18:29:21 -0700

    container

--- templates/dreamdinners/customer/home.tpl.php
+++ templates/dreamdinners/customer/home.tpl.php
@@ -14,31 +14,35 @@
 			</div>
 		</section>-->
 		<section>
-			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			  <ol class="carousel-indicators">
-				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-			  </ol>
-			  <div class="carousel-inner">
-				<div class="carousel-item active">
-				  <img class="d-block w-100" src="<?php echo IMAGES_PATH; ?>/landing_pages/new-guest-may24-free-shipping-delivery-1400x600.webp?auto=yes" alt="New Customer Exclusive Offer">
+			<div class="container my-2">
+				<div class="col-lg-12 mx-lg-auto">
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					  <ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					  </ol>
+					  <div class="carousel-inner">
+						<div class="carousel-item active">
+						  <img class="d-block w-100" src="<?php echo IMAGES_PATH; ?>/landing_pages/new-guest-may24-free-shipping-delivery-1400x600.webp?auto=yes" alt="New Customer Exclusive Offer">
+						</div>
+						<div class="carousel-item">
+						  <img class="d-block w-100" src="<?php echo IMAGES_PATH; ?>/landing_pages/steak-tacos-in-a-snap-headers-1400x600.webp?auto=yes" alt="Dinner in a Snap Carne Asada Steak Tacos">
+						</div>
+						<div class="carousel-item">
+						  <img class="d-block w-100" src="<?php echo IMAGES_PATH; ?>/landing_pages/travel-may24-1400x600.webp?auto=yes" alt="Travel with Dream Dinners">
+						</div>
+					  </div>
+					  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					  </a>
+					  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					  </a>
+					</div>
 				</div>
-				<div class="carousel-item">
-				  <img class="d-block w-100" src="<?php echo IMAGES_PATH; ?>/landing_pages/steak-tacos-in-a-snap-headers-1400x600.webp?auto=yes" alt="Dinner in a Snap Carne Asada Steak Tacos">
-				</div>
-				<div class="carousel-item">
-				  <img class="d-block w-100" src="<?php echo IMAGES_PATH; ?>/landing_pages/travel-may24-1400x600.webp?auto=yes" alt="Travel with Dream Dinners">
-				</div>
-			  </div>
-			  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			  </a>
-			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			  </a>
 			</div>
 		</section>
 	
