ae9d7ec52375303d8fb71b26ac88f19519aac672
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-04-23 18:56:24 -0700

    fluid container test

--- templates/dreamdinners/customer/home.tpl.php
+++ templates/dreamdinners/customer/home.tpl.php
@@ -14,7 +14,7 @@
 			</div>
 		</section>-->
 		<section>
-			<div class="container">
+			<div class="container-fluid">
 					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 					  <ol class="carousel-indicators">
 						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
