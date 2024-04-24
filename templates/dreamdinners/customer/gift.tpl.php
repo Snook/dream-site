b24b1adc8283d8995c6f5e221108ffec6bb003a7
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-04-24 16:06:43 -0700

    updated content

--- templates/dreamdinners/customer/gift.tpl.php
+++ templates/dreamdinners/customer/gift.tpl.php
@@ -7,7 +7,7 @@
 				<div class="container-fluid my-5">
 					<div class="row hero-double">
 						<div class="col-md-6 text-left p-5 my-5">
-							<h1 class="font-weight-bold mt-2">Gift Dinners to Friends and Family</h1>
+							<h1 class="class="font-weight-bold font-have-heart-two mt-2 font-size-extra-extra-large">Gift Dinners to Friends and Family</h1>
 							<p class="text-uppercase mb-4">Show you care by gifting your family and friends both near and far with easy, delicious meals. The perfect gift for any occasion like welcoming a new baby, a birthday, housewarming, or sympathy. Take something off their plate by having dinner ready to cook and delivered to their door.</p>
 							<!--<a href="/gift-card-order" class="btn btn-lg btn-green">Purchase Gift Card</a>-->
 						</div>
@@ -36,23 +36,24 @@
 								<div class="card-body">
 									<h5 class="card-title">Dinner for a Night</h5>
 									<p class="card-text">Add an extra meal or two to your monthly order to gift.</p>
-									<p><a href="/locations" class="btn btn-primary">Contact Your Local Store</a></p>
+									<p><a href="/locations" class="btn btn-primary">Contact Your Store</a></p>
 								</div>
 							</div>
 							<div class="card border-0 mx-1">
 								<img src="<?php echo IMAGES_PATH; ?>/landing_pages/chelsee-hood-458x344.webp" alt="Dream Dinners Home Delivery" class="img-fluid" />
 								<div class="card-body">
 									<h5 class="card-title">Delivered to their Door</h5>
-									<p class="card-text">Choose home delivery from one of our local stores to have three or more delicious dinners delivered to their home. *Available within 20 miles of most locations.</p>
-									<p><a href="/locations" class="btn btn-primary">Check Your Local Store</a></p>
+									<p class="card-text">Choose home delivery from one of our local stores to have three or more delicious dinners delivered to their home.</p>
+									<p><a href="/locations" class="btn btn-primary">Find a Local Store</a></p>
+									<p><i>*Available within 15 miles of most locations.</i></p>
 								</div>
 							</div>
 							<div class="card border-0 mx-2">
 								<img src="<?php echo IMAGES_PATH; ?>/landing_pages/dream-dinners-delivered-458x344.webp" alt="Dream Dinner Shipping" class="img-fluid" />
 								<div class="card-body">
 									<h5 class="card-title">Shipped to their Door</h5>
-									<p class="card-text">Show your love with four delicious, easy meals for them to enjoy. *Shipping available to select locations. Enter their zip code on the locations page.</p>
-									<p><a href="/locations" class="btn btn-primary">Check Gifting Areas</a></p>
+									<p class="card-text">Show your love with four delicious, easy meals for them to enjoy. *Enter their zip code to see if we can ship to them.</p>
+									<p><a href="/shipping" class="btn btn-primary">Ship a Box</a></p>
 								</div>
 							</div>
 							<div class="card border-0 mx-1">
