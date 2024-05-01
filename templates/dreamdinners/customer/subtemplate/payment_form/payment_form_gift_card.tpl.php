cd3d6a46bf4067d94b5ab8090e902c1fff29e06d
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-05-01 10:22:46 -0700

    Update style

--- templates/dreamdinners/customer/subtemplate/payment_form/payment_form_gift_card.tpl.php
+++ templates/dreamdinners/customer/subtemplate/payment_form/payment_form_gift_card.tpl.php
@@ -11,7 +11,14 @@
 					<input type="number" class="form-control" id="debit_gift_card_number" name="debit_gift_card_number" autocomplete="off" min="0" step="1" placeholder="Gift Card Number">
 				</div>
 				<div class="form-group col-4 col-xl-2">
-					<input type="number" class="form-control" id="debit_gift_card_amount" name="debit_gift_card_amount" min="0" step="0.01" placeholder="Amount">
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<div class="input-group-text">
+								$
+							</div>
+						</div>
+						<input type="number" class="form-control" id="debit_gift_card_amount" name="debit_gift_card_amount" min="0" step="0.01" placeholder="Amount">
+					</div>
 				</div>
 				<div class="form-group col-4 col-xl-2">
 					<button id="giftCardRedeem" class="btn btn-primary btn-block">Apply</button>
