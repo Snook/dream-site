ae6f2b25f16a2d06184b9b3f5081b092af85055a
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-05-13 18:38:46 -0700

    new what to expect emails

---
+++ templates/dreamdinners/email/what_to_expect_pickup.txt.php
@@ -0,0 +1,23 @@
+What to Expect
+
+Your Dream Dinners will be ready for pick up soon! We are so excited to help you with ready-to-cook meals that are easy and delicious. Here is what to expect when you arrive at your pick up.
+
+- Your meals will be ready on <b><?php echo $this->DAO_session->sessionStartDateTime()->format("F j, Y - g:i A"); ?> to <?php echo $this->DAO_session->sessionEndDateTime()->format("g:i A"); ?></b>.
+
+- Once you get home, place 3-4 meals in your fridge to thaw & enjoy this week. Add the extras to your freezer to enjoy next week. 
+
+- Ready to eat? Use our quick easy-to-follow cooking instructions including an estimated time-to-table to help plan your busy nights.
+
+- Forget to thaw dinner for the night? Grab a cook from frozen option or use these quick thaw tips.
+   - Submerge bag in cold tap water, changing the water every 30 min. Your meal should thaw in an hour or less.
+   - To speed things up, separate the bags of sauce and meat and place them directly in the cold water.	
+   - Once thawed, cook the meal immediately.
+
+- Looking to use the grill or your air fryer. Scan the QR code on the cooking instructions to see alternate cooking methods. 
+
+Enjoy your dinners with your family! When you need more meals, place another order at DreamDinners.com, you can order extras of your favorites or try something new. Our menu changes monthly, with at least 17 delicious recipes to choose from. 
+
+
+---------------------------
+
+If you have any questions, please reach out to us at <?php echo $this->DAO_orders->DAO_store->telephone_day; ?>.
