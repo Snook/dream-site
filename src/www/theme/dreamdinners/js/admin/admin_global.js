55bcbbbabd518a25b99bfa2cd08151f5b5d5d464
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-05-16 17:07:58 -0700

    Update support request link

--- src/www/theme/dreamdinners/js/admin/admin_global.js
+++ src/www/theme/dreamdinners/js/admin/admin_global.js
@@ -830,8 +830,10 @@ function handle_helpdesk()
 		e.preventDefault();
 
 		//var request_url = window.location.pathname + window.location.search;
-
-		location.href = "mailto:support@dreamdinners.com?subject=Support Request: " + encodeURIComponent(document.title) + "&body=%0D%0A%0D%0A%0D%0A%0D%0A%0D%0AProblem url: " + encodeURIComponent(window.location.href) + "%0D%0ABrowser: " + encodeURIComponent(window.navigator.userAgent);
+		window.open(
+			"mailto:support@dreamdinners.com?subject=Support Request: " + encodeURIComponent(document.title) + "&body=%0D%0A%0D%0A%0D%0A%0D%0A%0D%0AProblem url: " + encodeURIComponent(window.location.href) + "%0D%0ABrowser: " + encodeURIComponent(window.navigator.userAgent),
+			'_blank' // <- This is what makes it open in a new window.
+		);
 
 		return;
 
