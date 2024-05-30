64af1a96d0415cd5936fe9306493935c915dc441
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-05-29 17:45:03 -0700

    Update user digest

---
+++ reports/update_user_digest.php
@@ -0,0 +1,60 @@
+<?php
+require_once(dirname(__FILE__) . "/../includes/Config.inc");
+require_once("DAO/BusinessObject/COrders.php");
+require_once("DAO/BusinessObject/CUser.php");
+require_once("DAO/BusinessObject/CMenuItem.php");
+require_once("DAO/BusinessObject/CMenu.php");
+require_once("DAO/BusinessObject/CStore.php");
+require_once("DAO/CFactory.php");
+require_once("CLog.inc");
+
+set_time_limit(100000);
+ini_set('memory_limit', '-1');
+
+try
+{
+	$DAO_user_digest = DAO_CFactory::create('user_digest', true);
+	$DAO_user_digest->orderBy('user_digest.id DESC');
+	$DAO_user_digest->find();
+
+	while ($DAO_user_digest->fetch())
+	{
+		$org_DAO_user_digest = clone $DAO_user_digest;
+
+		$DAO_booking = DAO_CFactory::create('booking', true);
+		$DAO_booking->user_id = $DAO_user_digest->user_id;
+		$DAO_booking->status = CBooking::ACTIVE;
+		$DAO_session = DAO_CFactory::create('session', true);
+		$DAO_session->session_type = CSession::SPECIAL_EVENT;
+		$DAO_booking->joinAddWhereAsOn($DAO_session);
+		$DAO_booking->joinAddWhereAsOn(DAO_CFactory::create('orders', true));
+		$DAO_booking->orderBy('session.session_start ASC');
+		$DAO_booking->find();
+
+		while($DAO_booking->fetch())
+		{
+			if (empty($DAO_user_digest->order_id_first_pick_up) && $DAO_booking->DAO_session->isPickUp())
+			{
+				$DAO_user_digest->order_id_first_pick_up = $DAO_booking->order_id;
+			}
+
+			if (empty($DAO_user_digest->order_id_first_shipping) && $DAO_booking->DAO_orders->isShipping())
+			{
+				$DAO_user_digest->order_id_first_shipping = $DAO_booking->order_id;
+			}
+
+			if (empty($DAO_user_digest->order_id_first_home_delivery) && $DAO_booking->DAO_session->isDelivery())
+			{
+				$DAO_user_digest->order_id_first_home_delivery = $DAO_booking->order_id;
+			}
+		}
+
+		$DAO_user_digest->update($org_DAO_user_digest);
+	}
+
+}
+catch (exception $e)
+{
+
+}
+?>
