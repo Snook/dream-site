b53f2d4ff207ce2859d7984c5cbb73445a69ab22
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-06-03 14:19:12 -0700

    Show only current items available for box

--- templates/dreamdinners/customer/subtemplate/box_menu/box_menu_menu_cart_item_fixed.tpl.php
+++ templates/dreamdinners/customer/subtemplate/box_menu/box_menu_menu_cart_item_fixed.tpl.php
@@ -1,6 +1,8 @@
 <?php $DAO_menu_item = $item[key($item)]; ?>
-<div class="row mr-0 py-4" data-cart_menu_item="<?php echo $DAO_menu_item->menu_item_id; ?>">
-	<div class="col">
-		<div class="font-weight-bold font-size-small text-uppercase"><?php echo $DAO_menu_item->menu_item_name; ?></div>
+<?php if (!empty($DAO_menu_item->DAO_bundle_to_menu_item->current_offering)) { ?>
+	<div class="row mr-0 py-4" data-cart_menu_item="<?php echo $DAO_menu_item->menu_item_id; ?>">
+		<div class="col">
+			<div class="font-weight-bold font-size-small text-uppercase"><?php echo $DAO_menu_item->menu_item_name; ?></div>
+		</div>
 	</div>
-</div>
+<?php } ?>
