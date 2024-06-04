241a3f57f150624b6aebe3e564467670e1357ba9
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-06-03 17:40:19 -0700

    Update activity log

--- templates/dreamdinners/admin/subtemplate/store_activity/order_element.tpl.php
+++ templates/dreamdinners/admin/subtemplate/store_activity/order_element.tpl.php
@@ -3,7 +3,7 @@
 	<a href="/backoffice/user_details?id=<?php echo $item['user_id']; ?>" data-toggle="tooltip" title="<?php echo CUser::userTypeText($item['user_type']); ?>" target="_blank"><?php echo $item['user']; ?></a>
 	<b><?php echo $item['type']; ?></b>
 
-	<?php if ($item["digest_order_type"] == 'NEW') { ?>
+	<?php if ($item["digest_user_state"] == 'NEW') { ?>
 		<span class="badge badge-orange badge-pill">1<sup>st</sup> Order</span>
 	<?php } ?>
 
