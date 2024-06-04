fc7c85432f396507a0c9c9776ecd743bf238a1be
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-06-04 15:39:08 -0700

    Community location style update

--- templates/dreamdinners/customer/subtemplate/locations/locations_results_full.tpl.php
+++ templates/dreamdinners/customer/subtemplate/locations/locations_results_full.tpl.php
@@ -68,7 +68,8 @@
 								<div class="col-12 col-sm-5 col-md-6 mt-4 mt-sm-0">
 									<?php if ($store["type"] == 'COMMUNITY_PICK_UP') { ?>
 										<h3 class="text-uppercase font-weight-bold d-none d-sm-block">Community Pick Up location</h3>
-										<div class="text-uppercase d-none d-sm-block mb-4"><?php echo $store["DAO_store_pickup_location"]->location_title; ?></div>
+										<div class="text-uppercase mb-2"><?php echo $store["DAO_store_pickup_location"]->location_title; ?></div>
+										<div class="font-size-small mb-4">Provided by the <?php echo $store["DAO_store_pickup_location"]->DAO_store->store_name; ?> store</div>
 									<?php } else { ?>
 										<h3 class="text-uppercase font-weight-bold d-none d-sm-block mb-3"><?php echo $store["DAO_store"]->store_name; ?> Store</h3>
 									<?php } ?>
