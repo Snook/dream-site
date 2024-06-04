297f1971b7a77e6cad4c764029155f4d8c8e66eb
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-06-04 12:14:52 -0700

    Update search distance

--- templates/dreamdinners/customer/subtemplate/locations/locations_results_full.tpl.php
+++ templates/dreamdinners/customer/subtemplate/locations/locations_results_full.tpl.php
@@ -3,7 +3,7 @@
 	<form method="post" class="row">
 		<div class="col">
 			<h2 class="text-center mb-4">For the best value, visit a local store.</h2>
-			<?php if (!empty($this->zip_code)) { ?><p class="text-center">Search results for 45 miles around zip code <?php echo $this->zip_code; ?></p><?php } ?>
+			<?php if (!empty($this->zip_code)) { ?><p class="text-center">Search results for 30 miles around zip code <?php echo $this->zip_code; ?></p><?php } ?>
 			<?php foreach($this->store_results_array as $szState => $storArray ) { ?>
 				<?php $count = 1; foreach($storArray as $id => $store ) { $count++; ?>
 					<div class="row pb-4">
