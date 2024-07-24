8391e4b42944bf408e2a60a60a7d253008345f40
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-07-24 11:34:21 -0700

    Add updateMetricsForStoreIfNeeded test

--- test/test_CDashboardReport_updateMetrics.php
+++ test/test_CDashboardReport_updateMetrics.php
@@ -3,6 +3,9 @@ require_once(dirname(__FILE__) . "/../includes/Config.inc");
 require_once("CLog.inc");
 require_once('includes/CDashboardReport.inc');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 CDashboardNew::updateMetricsForStoreIfNeeded(store_id: 80, forceUpdate: true, testDate: false);
