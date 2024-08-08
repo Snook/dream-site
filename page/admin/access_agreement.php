8b9ebfb61510e3ee3db507996c28f8813229488c
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-08-08 16:18:47 -0700

    Clean up login

--- page/admin/access_agreement.php
+++ page/admin/access_agreement.php
@@ -1,150 +1,180 @@
 <?php
 require_once("includes/CPageAdminOnly.inc");
 
-class page_admin_access_agreement extends CPageAdminOnly {
+class page_admin_access_agreement extends CPageAdminOnly
+{
 
+	/**
+	 * @throws Exception
+	 */
 	function runNewEmployee()
 	{
 		$this->runAccessAgreement();
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	function runEventCoordinator()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runOpsLead()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runOpsSupport()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runDishwasher()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runManufacturerStaff()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runFranchiseOwner()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runFranchiseStaff()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runFranchiseLead()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runFranchiseManager()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runHomeOfficeStaff()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runHomeOfficeManager()
 	{
 		$this->runAccessAgreement();
 	}
+
+	/**
+	 * @throws Exception
+	 */
 	function runSiteAdmin()
 	{
 		$this->runAccessAgreement();
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	function runAccessAgreement()
 	{
-		$tpl = CApp::instance()->template();
-
-		$tpl->assign('back', '/backoffice/main');
-
-		if (!empty($_REQUEST['back']))
-		{
-			$tpl->assign('back', $_REQUEST['back']);
-		}
+		$this->Template->assign('back', '/backoffice');
 
 		if (!empty($_POST['agree_to_nda']) && !empty($_POST['agree_to_nda_submit']))
 		{
-			$Store = DAO_CFactory::create('store');
-			$Store->id = CBrowserSession::getCurrentFadminStore();
-			$Store->find(true);
+			$DAO_store = DAO_CFactory::create('store');
+			$DAO_store->id = CBrowserSession::getCurrentFadminStore();
+			$DAO_store->find(true);
 
-			$User = DAO_CFactory::create('user');
-			$User->id = CUser::getCurrentUser()->id;
+			$DAO_user = DAO_CFactory::create('user');
+			$DAO_user->id = CUser::getCurrentUser()->id;
 
-			if ($User->find(true))
+			if ($DAO_user->find(true))
 			{
-				$User->fadmin_nda_agree = 1;
-				$User->update();
+				$DAO_user->fadmin_nda_agree = 1;
+				$DAO_user->update();
 
-				CUserHistory::recordUserEvent($User->id, $Store->id, 'null', 700, 'null', 'null', 'Agreed to BackOffice NDA');
+				CUserHistory::recordUserEvent($DAO_user->id, $DAO_store->id, 'null', 700, 'null', 'null', 'Agreed to BackOffice NDA');
 
 				require_once('CMail.inc');
 
-				/* Notify if user has .local or .com email address */
-				list($account, $domain) = explode('@', $User->primary_email);
+				$Mail = new CMail();
 
-				if (strtolower($domain) == 'dreamdinners.com' || strtolower($domain) == 'dreamdinners.local')
+				$emailHTML = '<a href="' . HTTPS_SERVER . '/backoffice/user-details?id=' . $DAO_user->id . '">' . $DAO_user->firstname . ' ' . $DAO_user->lastname . '</a> has signed the BackOffice NDA.';
+
+				if (!$DAO_user->isUserGroupHomeOffice())
 				{
-					$Mail = new CMail();
-
-					$emailHTML = '<a href="' . HTTPS_SERVER . '/backoffice/user_details?id=' . $User->id . '">' . $User->firstname . ' ' . $User->lastname . '</a> has signed the BackOffice NDA.';
-
-					if ($User->user_type != CUser::SITE_ADMIN && $User->user_type != CUser::HOME_OFFICE_MANAGER && $User->user_type != CUser::HOME_OFFICE_STAFF)
-					{
-						$emailHTML .= ' <a href="' . HTTPS_SERVER . '/backoffice/store_details?id=' . $Store->id . '">' . $Store->store_name . '</a>.';
-					}
-
-					$Mail->send(ADMINISTRATOR_EMAIL,
-						ADMINISTRATOR_EMAIL,
-						'josh.thayer@dreamdinners.com',
-						'josh.thayer@dreamdinners.com',
-						$User->firstname . ' ' . $User->lastname . ' has signed the BackOffice NDA',
-						$emailHTML,
-						null,
-						'',
-						'',
-						null,
-						'admin_generic');
+					$emailHTML .= ' <a href="' . HTTPS_SERVER . '/backoffice/store-details?id=' . $DAO_store->id . '">' . $DAO_store->store_name . '</a>.';
 				}
 
-				if ($User->user_type == CUser::NEW_EMPLOYEE || $User->user_typw == CUser::DISHWASHER)
+				$Mail->send(ADMINISTRATOR_EMAIL, ADMINISTRATOR_EMAIL, 'josh.thayer@dreamdinners.com', 'josh.thayer@dreamdinners.com', $DAO_user->firstname . ' ' . $DAO_user->lastname . ' has signed the BackOffice NDA', $emailHTML, null, '', '', null, 'admin_generic');
+
+
+				if ($DAO_user->isUserType(user_type: CUser::NEW_EMPLOYEE) || $DAO_user->isUserType(user_type: CUser::DISHWASHER))
 				{
-					unset(CApp::instance()->template()->back);
-					CApp::bounce("/backoffice/safe_landing");
+					CApp::bounce("/backoffice/safe-landing");
 				}
 
-				CApp::bounce($tpl->back);
+				CApp::bounce($this->Template->back);
 			}
 			else
 			{
-				$tpl->setErrorMsg('Unexpected error, the user was not found, please contact support.');
+				$this->Template->setErrorMsg('Unexpected error, the user was not found, please contact support.');
 			}
 		}
 
 		if (CUser::getCurrentUser()->fadmin_nda_agree == 1 && empty($_GET['read_only']))
 		{
-			CApp::bounce($tpl->back);
+			CApp::bounce($this->Template->back);
 		}
 
-		$tpl->assign('hide_navigation', true);
+		$this->Template->assign('hide_navigation', true);
 
-		$tpl->assign('read_only', false);
+		$this->Template->assign('read_only', false);
 		if (!empty($_GET['read_only']))
 		{
-			$tpl->assign('read_only', true);
+			$this->Template->assign('read_only', true);
 		}
 
-		$tpl->assign('print_view', false);
+		$this->Template->assign('print_view', false);
 		if (!empty($_REQUEST['print_view']) && $_REQUEST['print_view'] == true)
 		{
-			$tpl->assign('print_view', true);
+			$this->Template->assign('print_view', true);
 		}
 	}
 }
-?>
