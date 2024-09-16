fbc7a4836a7cb6a67ec2eb3cffa55d704996ba59
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-09-16 13:33:33 -0700

    Remove Nextdoor, Pinterest, Adroll, TruConversion, Wisepops, Twitter, Wufoo, SurveyMonkey, Salesforce

--- templates/dreamdinners/customer/subtemplate/page_footer/page_footer_analytics.tpl.php
+++ templates/dreamdinners/customer/subtemplate/page_footer/page_footer_analytics.tpl.php
@@ -1,11 +1,4 @@
 <noscript>
 	<!--Facebook pixel-->
 	<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2559596927648235&ev=PageView&noscript=1" alt=""/>
-	<!--Nextdoor pixel-->
-	<img height="1" width="1" style="display:none" src="https://flask.nextdoor.com/pixel?pid=c729dacc-8cf3-4306-8042-7aa458ce5648&ev=PAGE_VIEW&noscript=1" alt=""/>
-	<?php if (CBrowserSession::getValue('dd_thank_you')) { ?>
-		<img height="1" width="1" style="display:none" src="https://flask.nextdoor.com/pixel?pid=c729dacc-8cf3-4306-8042-7aa458ce5648&ev=CONVERSION&noscript=1" alt=""/>
-	<?php } ?>
-	<!--Pinterest pixel-->
-	<img height="1" width="1" style="display:none;" alt="" src="https://ct.pinterest.com/v3/?event=init&tid=2614481882104&pd[em]=<?php echo $this->head_analytics_array['email_sha256']; ?>&noscript=1" />
 </noscript>
