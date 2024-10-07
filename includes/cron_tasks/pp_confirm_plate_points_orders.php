1543c759f0c4151a69d289f0d33858fb3a7195fa
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-10-07 15:39:22 -0700

    Disable platepoints info

--- includes/cron_tasks/pp_confirm_plate_points_orders.php
+++ includes/cron_tasks/pp_confirm_plate_points_orders.php
@@ -5,8 +5,8 @@ require_once("DAO/BusinessObject/CPointsUserHistory.php");
 require_once("DAO/CFactory.php");
 require_once("CLog.inc");
 
-try {
-
+try
+{
 	if (defined("DISABLE_CRON") && DISABLE_CRON)
 	{
 		CLog::Record("CRON: referral_rewards called but cron is disabled");
@@ -14,25 +14,29 @@ try {
 		exit;
 	}
 
+	if (CTemplate::formatDateTime(timeStamp: TIMENOW) >= '2024-11-07 00:00:00')
+	{
+		exit;
+	}
+
 	$Orders = DAO_CFactory::create('customer_referral');
 
 	$Orders->query("select b.order_id as order_id, s.session_start, s.id as session_id from booking b
 			join user u on u.id = b.user_id and u.dream_rewards_version = 3 and (u.dream_reward_status = 1 or u.dream_reward_status = 3) and u.is_deleted = 0
-			join session s on s.id = b.session_id and DATEDIFF(now(),s.session_start) > 6 and DATEDIFF(now(),s.session_start) < 45
+			join session s on s.id = b.session_id and s.menu_id <= 278 and DATEDIFF(now(),s.session_start) > 6 and DATEDIFF(now(),s.session_start) < 45
 			join orders o on o.id = b.order_id and o.is_in_plate_points_program = 1 and o.is_deleted = 0 and o.points_are_actualized = 0
 			where b.`status` = 'ACTIVE' and b.no_show = 0");
 
 	$totalCount = 0;
 	// Note: exception are handled, logged but not rethrown in the send_reminder_email function
 
-    $countFirstDDs = 0;
+	$countFirstDDs = 0;
 
-	while ( $Orders->fetch() ) {
-
+	while ($Orders->fetch())
+	{
 		$orderObj = DAO_CFactory::create('orders');
 		$orderObj->id = $Orders->order_id;
 		$orderObj->find(true);
-
 
 		$oldOrder = clone($orderObj);
 		$orderObj->points_are_actualized = 1;
@@ -41,25 +45,23 @@ try {
 		list($results, $platePointsStatus) = CPointsUserHistory::handleEvent($orderObj->user_id, CPointsUserHistory::ORDER_CONFIRMED, false, $orderObj);
 
 		if (isset($results))
+		{
 			$resultsArr[$orderObj->id] = $results;
+		}
 		else
+		{
 			$resultsArr[$orderObj->id] = array("no result");
+		}
 
 		CPointsUserHistory::clearLastOperationResult();
 
-
-
-
-        $totalCount++;
+		$totalCount++;
 	}
 
 	CLog::RecordCronTask($totalCount, CLog::SUCCESS, CLog::CONFIRM_PLATEPOINTS_ORDERS, "$totalCount order confirmations processed.");
-
-
 }
 catch (exception $e)
 {
-    CLog::RecordCronTask($totalCount, CLog::PARTIAL_FAILURE, CLog::CONFIRM_PLATEPOINTS_ORDERS, "expire_store_credit - warnings: Exception occurred: " . $e->getMessage());
+	CLog::RecordCronTask($totalCount, CLog::PARTIAL_FAILURE, CLog::CONFIRM_PLATEPOINTS_ORDERS, "expire_store_credit - warnings: Exception occurred: " . $e->getMessage());
 	CLog::RecordException($e);
 }
-?>
