b1b3ecde39e5e2ac8cbd435228e1ed8efd39fdfb
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-11-08 12:37:07 -0800

    updated store images and free recipe pages

--- templates/dreamdinners/customer/spec1a1.tpl.php
+++ templates/dreamdinners/customer/spec1a1.tpl.php
@@ -7,7 +7,7 @@
 	<header class="container my-5">
 		<div class="row">
 			<div class="col-12 text-center">
-				<h1>S’mores Popcorn</h1>
+				<h1>PB&amp;J Overnight Oats</h1>
 			</div>
 		</div>
 	</header>
@@ -19,13 +19,13 @@
 					<div class="row my-5">
 						<div class="col-md-6 mb-3 mt-5">
 							<div>
-								<img src="<?php echo IMAGES_PATH; ?>/landing_pages/smores-popcorn-475x450.webp" alt="Smores Popcorn" class="img-fluid" />
+								<img src="<?php echo IMAGES_PATH; ?>/landing_pages/pbj-oats-475x450.webp" alt="PBJ Overnight Oats" class="img-fluid" />
 							</div>
 						</div>
 						<div class="col-md-6 text-center mt-5">
 						  <img src="<?php echo IMAGES_PATH; ?>/landing_pages/great-pick-text.webp" alt="Great Pick" class="img-fluid" />
-						  <h2 class="font-weight-bold text-white">Download your S’mores Popcorn recipe here.</h2>
-						  <a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Popcorn-Recipe-Smores.pdf" target="_blank" class="btn btn-lg btn-green mb-5">Download Recipe</a>
+						  <h2 class="font-weight-bold text-white">Download your PB&amp;J Overnight Oats recipe here.</h2>
+						  <a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-PB&J-Overnight-Oats-Recipe.pdf" target="_blank" class="btn btn-lg btn-green mb-5">Download Recipe</a>
 						</div>
 						
 					</div>
@@ -37,14 +37,14 @@
 				<div class="container">
 					<div class="row my-5">
 						<div class="col-md-6 text-center mt-4">
-						  <h2 class="font-weight-bold font-have-heart-two font-size-extra-extra-large"><strong>Pairs Well With</strong></h2>
-							<h3>Slow Cooked BBQ Beef with Baby Roasters</h3>
-							<p>BBQ &amp; S’mores make a delicious combo. Tender ranch steaks slow cooked in a flavorful sweet BBQ sauce and served with roasted potatoes. </p>
+						  <h2 class="font-weight-bold font-have-heart-two font-size-extra-extra-large"><strong>Other Flavors We Think You Will Love</strong></h2>
+							<h3>Bacon Mac N' Three Cheese Bake</h3>
+							<p>Another classic flavor like PB&amp;J... Our elevated mac 'n cheese combines penne pasta in a three-cheese sauce, mixed with bacon, and topped with crunchy BBQ breadcrumbs. </p>
 						  <a href="/session_menu" class="btn btn-lg btn-primary btn-cyan-dark">ORDER NOW</a>
 						</div>
 						<div class="col-md-6 text-left">
 							<div>
-								<img src="<?php echo IMAGES_PATH; ?>/landing_pages/slow-cooked-bbq-beef-family-circle-458x344.webp" alt="Slow Cooked BBQ Beef with Baby Roasters" class="img-fluid" />
+								<img src="<?php echo IMAGES_PATH; ?>/landing_pages/bacon_mac_n_three_cheese_bake_circle-458x344.webp" alt="Bacon Mac N' Three Cheese Bake" class="img-fluid" />
 							</div>
 						</div>
 					</div>	
