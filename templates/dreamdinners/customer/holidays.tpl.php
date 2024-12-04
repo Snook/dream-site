32ee3c7bdb0f19cd1abc9b381cf22da99ccf4d96
Brandy Latta <141678256+brandylatta@users.noreply.github.com>
2024-12-04 09:44:08 -0800

    xmas

--- templates/dreamdinners/customer/holidays.tpl.php
+++ templates/dreamdinners/customer/holidays.tpl.php
@@ -24,27 +24,27 @@
 					<div class="col">
 						<div class="card-group text-center">
 							<div class="card border-0 mx-1">
-								<a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Turkey-Day-Toolkit.pdf" target="_blank"><img src="<?php echo IMAGES_PATH; ?>/landing_pages/thanksgiving-turkey-day-toolkit-458x344.webp" alt="Thanksgiving" class="img-fluid" /></a>
+								<a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Holiday-Toolkit.pdf" target="_blank"><img src="<?php echo IMAGES_PATH; ?>/landing_pages/holiday-toolkit-458x344.png" alt="Holiday" class="img-fluid" /></a>
 								<div class="card-body">
-									<h5 class="card-title">Thanksgiving Toolkit</h5>
-									<p class="card-text">Filled with activities, tips and tricks to keep the kiddos entertained.</p>
-									<p><a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Turkey-Day-Toolkit.pdf" target="_blank" class="btn btn-primary">Download Toolkit</a></p>
+									<h5 class="card-title">Holiday Toolkit</h5>
+									<p class="card-text">Games, activities, food, tips & tricks to fill your holidays with love & laughter.</p>
+									<p><a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Holiday-Toolkit.pdf" target="_blank" class="btn btn-primary">Download Toolkit</a></p>
 								</div>
 							</div>
 							<div class="card border-0 mx-1">
-								<a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Turkey-Day-Placemat.pdf" target="_blank"><img src="<?php echo IMAGES_PATH; ?>/landing_pages/thanksgiving-turkey-day-placemat-458x344.webp" alt="Thanksgiving Placemat" class="img-fluid" /></a>
+								<a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Holiday-Placemat.pdf" target="_blank"><img src="<?php echo IMAGES_PATH; ?>/landing_pages/holiday-placemat-458x344.png" alt="Holiday Placemat" class="img-fluid" /></a>
 								<div class="card-body">
-									<h5 class="card-title">Thanksgiving Placemat</h5>
+									<h5 class="card-title">Holiday Placemat</h5>
 									<p class="card-text">Download and print a 11x17 fun activity placemat.</p>
-									<p><a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Turkey-Day-Placemat.pdf" target="_blank" class="btn btn-primary">Download Placemat</a></p>
+									<p><a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Holiday-Placemat.pdf" target="_blank" class="btn btn-primary">Download Placemat</a></p>
 								</div>
 							</div>
 							<div class="card border-0 mx-1">
-								<a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Turkey-Day-Timeline.pdf" target="_blank"><img src="<?php echo IMAGES_PATH; ?>/landing_pages/thanksgiving-turkey-day-timeline-458x344.webp" alt="Thanksgiving" class="img-fluid" /></a>
+								<a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Holiday-Gift-Tags.pdf" target="_blank"><img src="<?php echo IMAGES_PATH; ?>/landing_pages/holiday-gift-tags-458x344.png" alt="Gift Tags" class="img-fluid" /></a>
 								<div class="card-body">
-									<h5 class="card-title">Thanksgiving Dinner Timeline</h5>
-									<p class="card-text">Timeline for effortless holiday meal prep.</p>
-									<p><a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Turkey-Day-Timeline.pdf" target="_blank" class="btn btn-primary">Download Timeline</a></p>
+									<h5 class="card-title">Holiday Gift Tags</h5>
+									<p class="card-text">Download some fun gift tags and fun gift giving ideas.</p>
+									<p><a href="https://dreamdinners.com/web_resources/media/Dream-Dinners-Holiday-Gift-Tags.pdf" target="_blank" class="btn btn-primary">Download Gift Tags</a></p>
 								</div>
 							</div>
 						</div>
