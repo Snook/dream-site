69b5015cdc6014728cc7000afc5677a7f4d311a9
Ryan Snook <105461991+Snook@users.noreply.github.com>
2024-12-31 11:06:17 -0800

    Update style

--- templates/dreamdinners/admin/page_footer.tpl.php
+++ templates/dreamdinners/admin/page_footer.tpl.php
@@ -12,7 +12,7 @@
 	<div id="footerlinks" class="footerlinks" style="text-align:center;">
 		<?php if (empty($this->hide_navigation)) { // hidden for some pages, ie admin_access_agreement ?>
 			<a href="/backoffice/resources">Resources</a> |
-			<a id="helpdesk_footer_link" href="mailto:support@dreamdinners.com" target="_blank" >Email Support</a> |
+			<a id="helpdesk_footer_link" href="mailto:support@dreamdinners.com" target="_blank" >Email Support</a>
 		<?php  } ?>
 	</div>
 
